d1ce27b360da942aabf8842ef1e16c14d347c45d
athenasj <30370786+athenasj@users.noreply.github.com>
2017-07-22 21:46:31 +0800

    Add files via upload

---
+++ keypadmicro1/keypadmicro1.ino
@@ -0,0 +1,29 @@
+#include <Keypad.h>
+
+const byte ROWS = 4; //four rows
+const byte COLS = 3; //three columns
+char keys[ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+      //pin of keypad 2,7,6,4 to
+byte rowPins[ROWS] = {3,8,7,5}; //connect to the row pinouts of the keypad 
+      //pin of keypad 3,1,5 to
+byte colPins[COLS] = {4,2,6}; //connect to the column pinouts of the keypad
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+void setup(){
+  Serial.begin(9600);
+}
+  
+void loop(){
+  char key = keypad.getKey();
+  
+  if (key){
+    Serial.println(key);
+  }
+}
+
